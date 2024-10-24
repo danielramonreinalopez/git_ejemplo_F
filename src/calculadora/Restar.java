46ef317a9fb96566bd0c68e831d93c021dd73482
DanielRamonReinaLopez <danielramon379@gmail.com>
2024-10-23 21:05:14 -0500

    resta

---
+++ src/calculadora/Restar.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author daniel
+ */
+public class Restar {
+    
+     public int  restar(int a, int b){
+        
+       int  c = a-b;
+       return c;
+    }
+    
+    
+}
